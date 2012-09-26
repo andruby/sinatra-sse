e530404d09faeb3c028f18f70d4f565fd4f0ef06
andruby <andrew@bedesign.be>
2012-09-25 18:21:58 -0700

    Working chat!

--- sinatra_sse.rb
+++ sinatra_sse.rb
@@ -3,7 +3,22 @@ require 'bundler'
 Bundler.require(:default, :development, ENV['RACK_ENV'])
 
 class SinatraSse < Sinatra::Base
+  set :server, 'thin'
+  @@connections = []
+
   get '/' do
     haml :index
+  end
+
+  get '/stream', :provides => 'text/event-stream' do
+    stream :keep_open do |out|
+      @@connections << out
+      out.callback { @@connections.delete(out) }
+    end
+  end
+
+  post '/' do
+    @@connections.each { |out| out << "data: #{params[:msg]}\n\n" }
+    204 # response without entity body
   end
 end
